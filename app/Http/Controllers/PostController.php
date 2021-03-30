5a13f513cf610bed5eae3c0b8dd8728abe7db761
kali <kali@kali.kali>
2021-03-30 12:46:11 -0400

    commit create, edit, show and paginate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,16 +3,82 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Http\Requests\StoreUpdatePost;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
 
-        $posts = Post::get();
+        //$posts = Post::orderBy('id', 'DESC')->paginate();
+        $posts = Post::latest()->paginate();
 
         //dd($posts);
 
     	return view('admin.posts.index', \compact('posts'));
+    }
+
+    public function create() {
+        return \view('admin.posts.create');
+    }
+
+    public function store(StoreUpdatePost $request)
+    {
+        Post::create($request->all());
+
+        return \redirect()
+            ->route('posts.index')
+            ->with('message', 'Post criado com Sucesso');
+    }
+
+    public function show($id)
+    {
+        //$post = Post::where('id', $id)->first();
+        $post = Post::find($id);
+
+        if (!$post) {
+            return \redirect()->route('posts.index');
+        }
+
+
+        return \view('admin.posts.show', \compact('post'));
+    }
+
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+
+        if (!$post) {
+            return \redirect()->route('posts.index');
+        }
+
+        $post->delete();
+
+        return \redirect()
+        ->route('posts.index')
+        ->with('message', 'Post Deletado com Sucesso');
+    }
+
+    public function edit($id)
+    {
+        if (!$post = Post::find($id)) {
+            return \redirect()->back();
+        }
+
+
+        return \view('admin.posts.edit', \compact('post'));
+    }
+
+    public function update(StoreUpdatePost $request, $id)
+    {
+        if (!$post = Post::find($id)) {
+            return \redirect()->back();
+        }
+
+        $post->update($request->all());
+
+        return \redirect()
+            ->route('posts.index')
+            ->with('message', 'Post atualizado com Sucesso');
     }
 }
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,23 @@
+<a href="{{ route('posts.create') }}">Criar novo post</a>
+<hr>
+@if (session('message'))
+    <div>
+        {{ session('message') }}
+    </div>
+@endif
+
 <h1>Post</h1>
 
 @foreach ($posts as $post)
-    <p>{{ $post->title }}</p>
+    <p>
+        {{ $post->title }} | {{ $post->content }}
+        [
+            <a href="{{ route('posts.show', $post->id) }}">Ver</a> |
+            <a href="{{ route('posts.edit', $post->id) }}">Editar</a>
+        ]
+    </p>
 @endforeach
+
+<hr>
+
+{{ $posts->links() }}
